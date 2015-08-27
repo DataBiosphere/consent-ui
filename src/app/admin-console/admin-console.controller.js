bf2eca3eb4e3a6d9bc5dc1cbe89b12422573e6e2
Nadya <nlopez@broadinstitute.org>
2015-08-27 15:40:18 -0300

    Upload Datasets UI

--- src/app/admin-console/admin-console.controller.js
+++ src/app/admin-console/admin-console.controller.js
@@ -5,13 +5,15 @@
         .controller('AdminConsole', AdminConsole);
 
     /* ngInject */
-    function AdminConsole($http, $modal, $state) {
+    function AdminConsole($http, $modal, $state, apiUrl) {
 
         var vm = this;
         vm.addDul = addDul;
         vm.addUser = addUser;
+        vm.addDataSets = addDataSets;
+        vm.downloadUrl = apiUrl + "consent/cases/datasets/datasets_spreadsheet";
 
-        function addDul () {
+        function addDul() {
 
             var modalInstance = $modal.open({
                 animation: false,
@@ -19,8 +21,8 @@
                 controller: 'DULModal',
                 controllerAs: 'DULModal',
                 resolve: {
-                           consent: new Object()
-                         }
+                    consent: new Object()
+                }
             });
 
             modalInstance.result.then(function () {//selectedItem - params to apply when the fc was successful
@@ -31,7 +33,24 @@
             });
         }
 
-        function addUser () {
+        function addUser() {
+
+            var modalInstance = $modal.open({
+                animation: false,
+                templateUrl: 'app/modals/modal-users/add-user/add-user-modal.html',
+                controller: 'ModalUsersAdd',
+                controllerAs: 'ModalUsersAdd'
+            });
+
+            modalInstance.result.then(function (selectedItem) {//selectedItem - params to apply when the fc was successful
+                //what to do if it was accepted
+                $state.go('admin_users');
+            }, function () {
+                //what to do if the modal was canceled
+            });
+        }
+
+        function addDataSets() {
 
             var modalInstance = $modal.open({
                 animation: false,
@@ -40,16 +59,9 @@
                 controllerAs: 'DataSetModal'
             });
 
-            //var modalInstance = $modal.open({
-            //    animation: false,
-            //     templateUrl: 'app/modals/modal-users/add-user/add-user-modal.html',
-            //     controller: 'ModalUsersAdd',
-            //     controllerAs: 'ModalUsersAdd'
-            //});
-
             modalInstance.result.then(function (selectedItem) {//selectedItem - params to apply when the fc was successful
                 //what to do if it was accepted
-                $state.go('admin_users');
+                $state.go('dataset_catalog');
             }, function () {
                 //what to do if the modal was canceled
             });
