9dbbda05b6b83927331b862df3e4d5ff5339ddab
nlopezzalba <nlopez@broadinstitute.org>
2015-10-19 16:23:06 -0300

    Several refinements on UI

--- src/app/modals/help-modal/help-modal.controller.js
+++ src/app/modals/help-modal/help-modal.controller.js
@@ -6,7 +6,7 @@
 
 
     /* ngInject */
-    function HelpModal($modalInstance, $scope, $state) {
+    function HelpModal($modalInstance, $state) {
 
         var vm = this;
 
@@ -19,36 +19,6 @@
         vm.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
-
-
-        /*****ALERTS*****/
-
-        $scope.alerts = [];
-
-        $scope.success = function (index) {
-            $scope.alerts.splice(index, 1);
-            $scope.alerts.push({
-                type: 'success',
-                title: 'Thanks for helping us!',
-                msg: 'Your comment was successfully sent. ',
-                alertType: 1
-            });
-        };
-
-        $scope.failure = function (index) {
-            $scope.alerts.splice(index, 1);
-            $scope.alerts.push({
-                type: 'danger',
-                title: 'Error!',
-                msg: "There has been a problem, your comment wasn't sent.",
-                alertType: 2
-            });
-        };
-
-        $scope.closeAlert = function (index) {
-            $scope.alerts.splice(index, 1);
-        };
-
 
     }
 })();
