619ac8ab648ef2076d194c6025a717d6694ee7c5
Diego Gil <diego.gil@maipucinos.com.ar>
2015-12-22 18:17:01 -0300

    Using a diff clientId

--- src/app/configuration/config.js
+++ src/app/configuration/config.js
@@ -1,4 +1,4 @@
 angular.module('ConsentConfiguration', [])
 .constant('apiUrl', "https://consent.dsde-consent-dev.broadinstitute.org/")
 .constant('ontologyApiUrl', "https://consent-ontology.dsde-consent-dev.broadinstitute.org/")
-.constant('clientId', "115985227940951446074");
+.constant('clientId', "555928665988-5bql5acjkoeniite54tenljet7b5r1ko.apps.googleusercontent.com");
