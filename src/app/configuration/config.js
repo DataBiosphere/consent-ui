6ba8ca237295cabf20e3a268788c5a034720a2f7
Diego Gil <diego.gil@maipucinos.com.ar>
2015-12-22 17:18:42 -0300

    Temp Fix for Vault config

---
+++ src/app/configuration/config.js
@@ -0,0 +1,4 @@
+angular.module('ConsentConfiguration', [])
+.constant('apiUrl', "https://consent.dsde-consent-dev.broadinstitute.org/")
+.constant('ontologyApiUrl', "https://consent-ontology.dsde-consent-dev.broadinstitute.org/")
+.constant('clientId', "115985227940951446074");
