7b1569930ca997b0b629e4528ce85c5757a2fc3e
Diego Gil <diego.gil@maipucinos.com.ar>
2016-02-26 15:51:23 -0300

    Fixing config

--- src/app/configuration/config.js
+++ src/app/configuration/config.js
@@ -1,4 +1,5 @@
 angular.module('ConsentConfiguration', [])
     .constant('apiUrl', "https://consent.dsde-consent-dev.broadinstitute.org/")
     .constant('ontologyApiUrl', "https://consent-ontology.dsde-consent-dev.broadinstitute.org/")
-    .constant('clientId', "555928665988-5bql5acjkoeniite54tenljet7b5r1ko.apps.googleusercontent.com");
+    .constant('clientId', "555928665988-5bql5acjkoeniite54tenljet7b5r1ko.apps.googleusercontent.com")
+    .constant('gwasUrl', "http://epi.grants.cancer.gov/dac/examples.html");
