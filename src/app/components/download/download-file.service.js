3c76c12b14f776026d7c0307c26ee61afa8797e0
Greg Rushton <rushtong@users.noreply.github.com>
2016-06-28 06:58:50 -0400

    6/28 Release (#113)
    * disable cache on ie11, download files, dul link fixes
    
    * added expires header
    
    * configure proxy cache header

--- src/app/components/download/download-file.service.js
+++ src/app/components/download/download-file.service.js
@@ -6,20 +6,11 @@
         .factory('downloadFileService', function () {
             return {
                 downloadFile: function (fileName, text) {
-
-                    var isFirefox = typeof InstallTrigger !== 'undefined';
-                    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
                     var isIE = /*@cc_on!@*/false || !!document.documentMode;
-                    var isEdge = !isIE && !!window.StyleMedia;
-                    var isChrome = !!window.chrome && !!window.chrome.webstore;
-                    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-                    var isBlink = (isChrome || isOpera) && !!window.CSS;
-
                     var blob = new Blob([text], { type: 'text/plain' });
                     var downloadElement = angular.element('<a/>');
                     downloadElement.css({ display: 'none' });
                     angular.element(document.body).append(downloadElement);
-
                     if (isIE) {
                         downloadElement.attr({
                             href: window.navigator.msSaveOrOpenBlob(blob, fileName),
