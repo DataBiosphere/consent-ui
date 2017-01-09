75cb3b20b21b3786378530c0b6e173a2467394ca
Greg Rushton <grushton@broadinstitute.org>
2017-01-09 14:38:55 -0500

    trying to split configs out.

--- src/app/configuration/config.js
+++ src/app/configuration/config.js
@@ -1,5 +1,5 @@
 angular.module('ConsentConfiguration', [])
-    .constant('apiUrl', "https://consent.dsde-consent-dev.broadinstitute.org/api/")
-    .constant('ontologyApiUrl', "https://consent-ontology.dsde-consent-dev.broadinstitute.org/")
-    .constant('clientId', "555928665988-5bql5acjkoeniite54tenljet7b5r1ko.apps.googleusercontent.com")
-    .constant('gwasUrl', "http://epi.grants.cancer.gov/dac/examples.html");
+    .value('apiUrl', "")
+    .value('ontologyApiUrl', "")
+    .value('clientId', "")
+    .value('gwasUrl', "");
