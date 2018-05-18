3334625f1b04f87d301629de4d27e9362b0c5c34
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2018-05-18 14:53:04 -0300

    election number; search combined fields

--- src/app/reviewed-cases/reviewed-cases.controller.js
+++ src/app/reviewed-cases/reviewed-cases.controller.js
@@ -9,30 +9,35 @@
 
         var vm = this;
         vm.electionsList = {'dul': [], 'access': []};
-        
+
         if($stateParams.menu !== true && $rootScope.currentDulPage !== undefined){
              $scope.currentDulPage = $rootScope.currentDulPage;
              $rootScope.currentDulPage = 1;
         } else{
             $rootScope.currentDulPage = 1;
-            $scope.currentDulPage = $rootScope.currentDulPage; 
+            $scope.currentDulPage = $rootScope.currentDulPage;
         }
-        
+
         $scope.$watch('currentDulPage', function(num) {
             $rootScope.currentDulPage = num;
         });
-        
+
         if($stateParams.menu !== true && $rootScope.currentDarPage !== undefined){
              $scope.currentDarPage = $rootScope.currentDarPage;
              $rootScope.currentDarPage = 1;
         } else{
             $rootScope.currentDarPage = 1;
-            $scope.currentDarPage = $rootScope.currentDarPage; 
+            $scope.currentDarPage = $rootScope.currentDarPage;
         }
-        
+
         $scope.$watch('currentDarPage', function(num) {
             $rootScope.currentDarPage = num;
         });
+
+        $scope.sort = function(keyname){
+            $scope.sortBy = keyname;
+            $scope.reverse = !$scope.reverse;
+        };
 
         init();
 
@@ -40,6 +45,13 @@
             vm.electionsList.dul = transformElectionResultData(reviewedConsents);
             vm.electionsList.access = transformElectionResultData(reviewedDRs);
 
+            var regex = new RegExp('-', 'g');
+            vm.electionsList.dul.forEach(function(election) {
+                var str = election.displayId;
+                str = str.replace(regex, ' ');
+                election.ct = String(election.displayId) + ' ' + String(election.version < 10 ? '0' + election.version : election.version);
+                election.cts = str + ' ' + String(election.version < 10 ? '0' + election.version : election.version);
+            });
         }
     }
 
