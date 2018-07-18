03ee708a1e5aa7c37dcd77c509ded4c63387e1f7
leoforconesi <lforcone@broadinstitute.org>
2018-07-18 12:52:40 -0300

    use consent name in final access review

--- src/app/review-results/final-access-review-results.controller.js
+++ src/app/review-results/final-access-review-results.controller.js
@@ -315,7 +315,6 @@
 
             cmElectionService.findDataAccessElectionReview(electionId, false).$promise.then(function (data) {
                 showAccessData(data);
-                $scope.consentName = data.consent.consentId;
                 cmElectionService.findRPElectionReview(electionId, false).
                 $promise.then(function (data) {
                     if(data.election !== undefined){
@@ -331,7 +330,7 @@
                         $scope.showRPaccordion = false;
                     }
                 });
-
+                $scope.consentName = data.associatedConsent.name
                 cmElectionService.findElectionReviewById(data.associatedConsent.electionId, data.associatedConsent.consentId).$promise.then(function (data) {
                     $scope.electionReview = data;
                     showDULData(data);
