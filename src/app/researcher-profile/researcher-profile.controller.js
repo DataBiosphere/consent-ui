e42dee96460e53f67dacd35c6a243e732073609d
vernicavicario <vernicavicario@yahoo.com.ar>
2018-08-02 14:10:23 -0300

    format code

--- src/app/researcher-profile/researcher-profile.controller.js
+++ src/app/researcher-profile/researcher-profile.controller.js
@@ -119,7 +119,7 @@
             IN.API.Raw("/people/~:(public-profile-url)").result(onSuccess).error(onError);
         }
 
-                // Handle an error response from the API call
+        // Handle an error response from the API call
         function onError() {
             $scope.formData.linkedinProfile = undefined;
         }
