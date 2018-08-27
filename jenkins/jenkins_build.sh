5f814e006dd9daeed25c33b83b40130a0c74b3e6
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-08-27 14:27:48 -0400

    fix jenkins command

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 set -eux
 SVCACCT_FILE="dspci-wb-gcr-service-account.json"
 GCR_SVCACCT_VAULT="secret/dsde/dsp-techops/common/$SVCACCT_FILE"
@@ -10,7 +9,7 @@ docker run --rm  -e VAULT_TOKEN=$VAULT_TOKEN \
    broadinstitute/dsde-toolbox:latest vault read --format=json ${GCR_SVCACCT_VAULT} \
    | jq .data > ${SVCACCT_FILE}
 
-./build.sh jar -d push -g gcr.io/broad-dsp-gcr-public/${PROJECT} -k ${SVCACCT_FILE}
+./build.sh -d push -g gcr.io/broad-dsp-gcr-public/${PROJECT} -k ${SVCACCT_FILE}
 
 # clean up
 rm -f ${SVCACCT_FILE}
